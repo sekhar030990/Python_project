afcf13c152b9e9d90cebea9ef082406246854f56
raja <sekhar030990@gmail.com>
2021-04-25 09:00:14 +0530

    changes done create_testcase

--- 1_create_testcase.py
+++ 1_create_testcase.py
@@ -13,7 +13,7 @@ def test_verify_sub():
     r = sub(10.5,5.3)
     assert r == 5.2 , ' \n Sub fun is not working as expected'
     r = sub(-10,-5)
-    assert r == -5 ,' \n Sub fun is not working as expected'
+    assert r == 10 ,' \n Sub fun is not working as expected'
     print(' \n\n sub fun Ran successfully')
 
 test_verify_sub()
